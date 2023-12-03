3bc072f308cefc9a6846eedf80d63f60a30376c3
Martin-Garro <144090810+Martin-Garro@users.noreply.github.com>
2023-12-02 23:27:35 -0300

    Ayuda lauty

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import ListProjects from './components/ProjectComponents/ListProjects';
+import ProjectDetails from './components/ProjectComponents/ProjectDetails';
+
+function App() {
+  const projects = [
+    {
+      id: 1,
+      name: "Proyecto 1",
+      description: "Descripción del Proyecto 1",
+      icon: "icon-1.png",
+      members: [101, 102, 103],
+      epics: ["Epic A", "Epic B"],
+    },
+    {
+      id: 2,
+      name: "Proyecto 2",
+      description: "Descripción del Proyecto 2",
+      icon: "icon-2.png",
+      members: [104, 105, 106],
+      epics: ["Epic X", "Epic Y"],
+    },
+  ];
+
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/my-projects" render={() => <ListProjects projects={projects} />} />
+          <Route path="/my-projects/:projectId" exact render={() => <ProjectDetails projects={projects} />} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/ProjectComponents/ProjectDetails.jsx
@@ -0,0 +1,29 @@
+// src/components/ProjectComponents/ProjectDetails.js
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const ProjectDetails = ({ projects }) => {
+  const { projectId } = useParams();
+  const project = projects.find((p) => String(p.id) === projectId);
+
+  if (!project) {
+    return <p>Proyecto no encontrado</p>;
+  }
+
+  return (
+    <div>
+      <h2>{project.name} - Detalles del Proyecto</h2>
+      <p>{project.description}</p>
+      <h3>Epics:</h3>
+      <ul>
+        {project.epics ? (
+          project.epics.map((epic, index) => <li key={index}>{epic}</li>)
+        ) : (
+          <li>No hay Epics definidas para este proyecto.</li>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default ProjectDetails;
